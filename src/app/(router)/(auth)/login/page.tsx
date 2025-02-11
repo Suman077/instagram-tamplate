9dcb14ff7776d332fa98a2eaa30cd8f26e7e5ce6
Suman077 <safaralizoda1102007@gmail.com>
2025-02-12 00:07:25 +0500

    comment

--- src/app/(router)/(auth)/login/page.tsx
+++ src/app/(router)/(auth)/login/page.tsx
@@ -1,5 +1,24 @@
-export default function PageLogin(){
-  return <>
-    <h1> page login</h1>
-  </>
+"use client"
+import { LoginForm } from "@/shared/ui/login-form"
+export default function LoginPage() {
+  return (
+    <div className="mt-[120px]">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex justify-center gap-2 md:justify-start">
+        </div>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <LoginForm />
+          </div>
+        </div>
+      </div>
+      <div className="relative hidden bg-muted lg:block">
+        <img
+          src="/placeholder.svg"
+          alt="Image"
+          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
+    </div>
+  )
 }
--- src/app/(router)/(auth)/registration/page.tsx
+++ src/app/(router)/(auth)/registration/page.tsx
@@ -1,5 +1,23 @@
-export default function PageRegistration(){
-  return <>
-     <h1>Page Registration</h1>
-  </>
+import {RegistrationForm} from "@/shared/ui/registration-form"
+export default function PageRegistration() {
+  return (
+    <div className="mt-[120px]">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex justify-center gap-2 md:justify-start">
+        </div>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+         < RegistrationForm/>
+          </div>
+        </div>
+      </div>
+      <div className="relative hidden bg-muted lg:block">
+        <img
+          src="/placeholder.svg"
+          alt="Image"
+          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
+    </div>
+  )
 }
